f173bbc02f0df7c8e942975b2f67d31f7c3e7257
Sunho Kim <ksunhokim123@naver.com>
2020-08-07 04:42:50 +0900

    A32: Implement thumb32 ADC (imm) instruction

--- tests/A32/fuzz_thumb.cpp
+++ tests/A32/fuzz_thumb.cpp
@@ -360,6 +360,9 @@ TEST_CASE("Fuzz Thumb2 instructions set 1", "[JitX64][Thumb2]") {
         Thumb32InstGen("11110i01000Snnnn0kkkddddmmmmmmmm", // ADD (imm)
                  [](u32 inst){ return Common::Bits<8, 11>(inst) != 0b1111 && Common::Bits<16, 19>(inst) != 0b1111; }),
             // R15 is UNPREDICTABLE
+        Thumb32InstGen("11110i01010Snnnn0kkkddddmmmmmmmm", // ADC (imm)
+             [](u32 inst){ return Common::Bits<8, 11>(inst) != 0b1111 && Common::Bits<16, 19>(inst) != 0b1111; }),
+            // R15 is UNPREDICTABLE
         
     };
 
