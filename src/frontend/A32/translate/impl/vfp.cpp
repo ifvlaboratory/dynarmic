d8da57d63b1c672832a0b11e211dac19e71117d3
Banny <zhkl0228@gmail.com>
2020-10-31 23:17:52 +0800

    A32: Implement thumb32 vfp_VSTM_a1

--- src/frontend/A32/translate/impl/vfp.cpp
+++ src/frontend/A32/translate/impl/vfp.cpp
@@ -1890,6 +1890,57 @@ bool ArmTranslatorVisitor::vfp_VSTM_a1(Cond cond, bool p, bool u, bool D, bool w
     return true;
 }
 
+// VSTM{mode}<c> <Rn>{!}, <list of double registers>
+bool ThumbTranslatorVisitor::vfp_VSTM_a1(bool p, bool u, bool D, bool w, Reg n, size_t Vd, Imm<8> imm8) {
+    if (!ConditionPassed()) {
+        return true;
+    }
+    if (!p && !u && !w) {
+        ASSERT_MSG(false, "Decode error");
+    }
+
+    if (p && !w) {
+        ASSERT_MSG(false, "Decode error");
+    }
+
+    if (p == u && w) {
+        return thumb32_UDF();
+    }
+
+    if (n == Reg::PC) {
+        return UnpredictableInstruction();
+    }
+
+    const auto d = ToExtReg(true, Vd, D);
+    const size_t regs = imm8.ZeroExtend() / 2;
+
+    if (regs == 0 || regs > 16 || A32::RegNumber(d)+regs > 32) {
+        return UnpredictableInstruction();
+    }
+
+    const u32 imm32 = imm8.ZeroExtend() << 2U;
+    auto address = u ? ir.GetRegister(n) : IR::U32(ir.Sub(ir.GetRegister(n), ir.Imm32(imm32)));
+    if (w) {
+        ir.SetRegister(n, u ? IR::U32(ir.Add(address, ir.Imm32(imm32))) : address);
+    }
+    for (size_t i = 0; i < regs; i++) {
+        const auto value = ir.GetExtendedRegister(d + i);
+        auto word1 = ir.LeastSignificantWord(value);
+        auto word2 = ir.MostSignificantWord(value).result;
+
+        if (ir.current_location.EFlag()) {
+            std::swap(word1, word2);
+        }
+
+        ir.WriteMemory32(address, word1);
+        address = ir.Add(address, ir.Imm32(4));
+        ir.WriteMemory32(address, word2);
+        address = ir.Add(address, ir.Imm32(4));
+    }
+
+    return true;
+}
+
 // VSTM{mode}<c> <Rn>{!}, <list of single registers>
 bool ArmTranslatorVisitor::vfp_VSTM_a2(Cond cond, bool p, bool u, bool D, bool w, Reg n, size_t Vd, Imm<8> imm8) {
     if (!p && !u && !w) {
