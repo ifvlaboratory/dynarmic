79b18e6126be7149dd1ef1b16ff62e7e9aef7297
Banny <zhkl0228@gmail.com>
2020-10-31 22:21:10 +0800

    A32: Implement thumb32 vfp_VDIV

--- src/frontend/A32/translate/impl/vfp.cpp
+++ src/frontend/A32/translate/impl/vfp.cpp
@@ -278,6 +278,25 @@ bool ArmTranslatorVisitor::vfp_VDIV(Cond cond, bool D, size_t Vn, size_t Vd, boo
     });
 }
 
+// VDIV<c>.F64 <Dd>, <Dn>, <Dm>
+// VDIV<c>.F32 <Sd>, <Sn>, <Sm>
+bool ThumbTranslatorVisitor::vfp_VDIV(bool D, size_t Vn, size_t Vd, bool sz, bool N, bool M, size_t Vm) {
+    if (!ConditionPassed()) {
+        return true;
+    }
+
+    const auto d = ToExtReg(sz, Vd, D);
+    const auto n = ToExtReg(sz, Vn, N);
+    const auto m = ToExtReg(sz, Vm, M);
+
+    return EmitVfpVectorOperation(sz, d, n, m, [this](ExtReg d, ExtReg n, ExtReg m) {
+        const auto reg_n = ir.GetExtendedRegister(n);
+        const auto reg_m = ir.GetExtendedRegister(m);
+        const auto result = ir.FPDiv(reg_n, reg_m);
+        ir.SetExtendedRegister(d, result);
+    });
+}
+
 // VFNMS<c>.F64 <Dd>, <Dn>, <Dm>
 // VFNMS<c>.F32 <Sd>, <Sn>, <Sm>
 bool ArmTranslatorVisitor::vfp_VFNMS(Cond cond, bool D, size_t Vn, size_t Vd, bool sz, bool N, bool M, size_t Vm) {
